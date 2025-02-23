e39bb47eabf175a182c5bf0b217ee887693734f9
Nikiktoss <mikita.chebotarevskiy@gmail.com>
2025-02-23 20:33:40 +0300

    Add orjson library

--- backend/src/core/exceptions/validation_exception.py
+++ backend/src/core/exceptions/validation_exception.py
@@ -2,8 +2,8 @@ import structlog
 from fastapi import status
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
+from fastapi.responses import ORJSONResponse
 from starlette.requests import Request
-from starlette.responses import JSONResponse
 
 from src.core.config.settings import settings
 from src.core.exceptions.base import ApplicationException
@@ -11,7 +11,7 @@ from src.core.exceptions.base import ApplicationException
 LOG = structlog.stdlib.get_logger()
 
 
-async def custom_validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+async def custom_validation_exception_handler(request: Request, exc: RequestValidationError) -> ORJSONResponse:
     _ = request
     if settings.DEBUG:
         LOG.error(
@@ -19,7 +19,7 @@ async def custom_validation_exception_handler(request: Request, exc: RequestVali
             exc_info=exc,
         )
 
-    return JSONResponse(
+    return ORJSONResponse(
         ApplicationException(
             detail="Request Body Validation error",
             errors=jsonable_encoder(exc.errors()),
