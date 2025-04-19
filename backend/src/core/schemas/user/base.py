72911f6555791d101cb5203e85ab0fc97ebd32e7
Nikiktoss <mikita.chebotarevskiy@gmail.com>
2025-04-19 15:02:44 +0200

    Refactor user schemas

--- backend/src/core/schemas/user/base.py
+++ backend/src/core/schemas/user/base.py
@@ -1,11 +1,14 @@
-import re
 from datetime import date, datetime
+from typing import Annotated
 from uuid import UUID
 
-from pydantic import BaseModel, ConfigDict, EmailStr, field_validator
+from pydantic import AfterValidator, BaseModel, ConfigDict, EmailStr
 
 from src.core.enums import GenderTypeEnum, UserRoleTypeEnum
-from src.core.schemas.common_validators import validate_day_is_not_previous
+from src.core.schemas.user.validators import (
+    check_password,
+    validate_unavailable_dates,
+)
 
 
 class UserBase(BaseModel):
@@ -18,14 +21,17 @@ class UserBase(BaseModel):
     user_roles: list[UserRoleTypeEnum]
 
 
-class UserRead(UserBase):
-    model_config = ConfigDict(from_attributes=True)
-
-    id: UUID
+class UserAdditionalInfo(BaseModel):
     photo: str | None = None
     viber_link: str | None = None
     telegram_link: str | None = None
     instagram_link: str | None = None
+
+
+class UserRead(UserBase, UserAdditionalInfo):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID
     unavailable_dates: list[date] | None = None
     created_at: datetime
     updated_at: datetime
@@ -33,23 +39,9 @@ class UserRead(UserBase):
     # TODO: add theatrical_roles
 
 
-class UserCreate(UserBase):
-    password: str
-    photo: str | None = None
-    viber_link: str | None = None
-    telegram_link: str | None = None
-    instagram_link: str | None = None
-    unavailable_dates: list[date] | None = None
-
-    @field_validator("unavailable_dates", mode="after")
-    def validate_unavailable_dates(cls, value: list[date] | None) -> list[date] | None:
-        if value:
-            for free_date in value:
-                validate_day_is_not_previous(
-                    day=free_date, error_msg=""" Field "unavailable_dates" cannot contain previous days """
-                )
-
-        return value
+class UserCreate(UserBase, UserAdditionalInfo):
+    password: Annotated[str, AfterValidator(check_password)]
+    unavailable_dates: Annotated[list[date] | None, AfterValidator(validate_unavailable_dates)] = None
 
 
 class UserCreateWithOAuth(UserCreate):
@@ -58,7 +50,7 @@ class UserCreateWithOAuth(UserCreate):
     user_roles: list[UserRoleTypeEnum] = [UserRoleTypeEnum.VIEWER.value]
 
 
-class UserUpdate(UserCreate):
+class UserUpdate(UserBase, UserAdditionalInfo):
     first_name: str | None = None
     last_name: str | None = None
     gender: GenderTypeEnum | None = None
@@ -66,23 +58,8 @@ class UserUpdate(UserCreate):
     birth_date: date | None = None
     email: EmailStr | None = None
     user_roles: list[UserRoleTypeEnum] | None = None
-    password: str | None = None
+    unavailable_dates: Annotated[list[date] | None, AfterValidator(validate_unavailable_dates)] = None
 
 
 class UserRegister(UserBase):
-    password: str
-
-    @field_validator("password", mode="after")
-    def check_password(cls, value: str) -> str:
-        if len(value) < 8:
-            raise ValueError("Password must be at least 8 characters long")
-        if not re.search(r"[A-Z]", value):
-            raise ValueError("Password must contain at least one uppercase letter")
-        if not re.search(r"[a-z]", value):
-            raise ValueError("Password must contain at least one lowercase letter")
-        if not re.search(r"[0-9]", value):
-            raise ValueError("Password must contain at least one digit")
-        if not re.search(r"[\W_]", value):
-            raise ValueError("Password must contain at least one special character")
-
-        return value
+    password: Annotated[str, AfterValidator(check_password)]
